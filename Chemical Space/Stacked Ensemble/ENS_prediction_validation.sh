34ec6b2e63e1ca7149a1ab266851eccd00681ddc
neeraj-24 <81298448+neeraj-24@users.noreply.github.com>
2021-04-20 15:45:23 +0530

    Delete Chemical Space directory

--- Chemical Space/Stacked Ensemble/ENS_prediction_validation.sh
+++
@@ -1,7 +0,0 @@
-awk -F, '{print $1, "\t" $2}' validation_set_refined_according_to_model.csv > 1
-awk -F, '{print $1}' ENS_predicted_indepenednt_test.csv > 2 
-paste 1 2 > 1_2 | sed 's/\t/ /g' 1_2 | sed 's/"//g' 1_2 > 3 
-awk '{if ($2==1 && $3 == 1) print $0}' 3 > Identified_hits_for_independent_set.txt
-
-#removing free files
-rm -rf 1 2 3 1_2
